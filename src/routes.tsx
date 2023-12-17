35353b5aa8129e5b07de7c9e8497d4a5d612f9e0
Lev Eidelman Nagar <lev.eidelman@linearb.io>
2023-12-17 16:48:29 +0200

    path aliases

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
-import { Root } from './routes/Root';
-import { About } from './routes/About';
-import { Help } from './routes/Help';
+import { About } from 'routes/About';
+import { Root } from 'routes/Root';
+import { Help } from 'routes/Help';
 import type { RouteObject } from 'react-router-dom';
 
 export const routes: RouteObject[] = [
